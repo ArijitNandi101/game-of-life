40837408281dde9a327ab31f7c75b8ec1d8b71d4
ArijitNandi101 <arijitn001@gmail.com>
2021-09-15 14:49:27 +0530

    task: set up project structure and initial dependencies

--- src/test/java/com/makkajai/dev/challenge/ApplicationMainTest.java
+++ src/test/java/com/makkajai/dev/challenge/ApplicationMainTest.java
@@ -1,19 +1,19 @@
 package com.makkajai.dev.challenge;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+class ApplicationMainTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
