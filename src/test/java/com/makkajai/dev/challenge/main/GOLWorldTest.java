62d85778b6c59ce70a32834d0b91a0cbd1e792d4
ArijitNandi101 <arijitn001@gmail.com>
2021-09-25 22:59:14 +0530

    task: added GOLEntity test for adding neighbour connctions

--- src/test/java/com/makkajai/dev/challenge/main/GOLWorldTest.java
+++ src/test/java/com/makkajai/dev/challenge/main/GOLWorldTest.java
@@ -4,15 +4,16 @@ import com.makkajai.dev.challenge.ds.planar.Vec2i;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GOLWorldTest {
 
     @Test
     void testConstructor() {
-        GOLWorld GOLWorld = new GOLWorld();
+        GOLWorld golWorld = new GOLWorld();
 
-        assertEquals(0, GOLWorld.getEntities().size());
+        assertEquals(0, golWorld.getEntities().size());
     }
 
     @Test
@@ -20,18 +21,18 @@ public class GOLWorldTest {
         GOLEntity e1 = new GOLEntity(new Vec2i(2, 7));
         GOLEntity e2 = new GOLEntity(new Vec2i(-2, 5));
         GOLEntity e3 = new GOLEntity(new Vec2i(2, 7));
-        GOLWorld GOLWorld = new GOLWorld();
-        GOLWorld.addEntity(e1);
-        assertEquals(1, GOLWorld.getEntities().size());
+        GOLWorld golWorld = new GOLWorld();
+        golWorld.addEntity(e1);
+        assertEquals(1, golWorld.getEntities().size());
 
-        GOLWorld.addEntity(e2);
-        assertEquals(2, GOLWorld.getEntities().size());
+        golWorld.addEntity(e2);
+        assertEquals(2, golWorld.getEntities().size());
 
-        GOLWorld.addEntity(e3);
-        assertEquals(2, GOLWorld.getEntities().size());
+        golWorld.addEntity(e3);
+        assertEquals(2, golWorld.getEntities().size());
 
-        GOLWorld.addEntity(e1);
-        assertEquals(2, GOLWorld.getEntities().size());
+        golWorld.addEntity(e1);
+        assertEquals(2, golWorld.getEntities().size());
     }
 
     @Test
@@ -39,20 +40,20 @@ public class GOLWorldTest {
         GOLEntity e1 = new GOLEntity(new Vec2i(2, 7));
         GOLEntity e2 = new GOLEntity(new Vec2i(-2, 5));
         GOLEntity e3 = new GOLEntity(new Vec2i(2, 7));
-        GOLWorld GOLWorld = new GOLWorld();
-        GOLWorld.addEntity(e1);
-        GOLWorld.addEntity(e2);
-        GOLWorld.addEntity(e3);
-        assertEquals(2, GOLWorld.getEntities().size());
+        GOLWorld golWorld = new GOLWorld();
+        golWorld.addEntity(e1);
+        golWorld.addEntity(e2);
+        golWorld.addEntity(e3);
+        assertEquals(2, golWorld.getEntities().size());
 
-        GOLWorld.removeEntity(e2);
-        assertEquals(1, GOLWorld.getEntities().size());
+        golWorld.removeEntity(e2);
+        assertEquals(1, golWorld.getEntities().size());
 
-        GOLWorld.removeEntity(e1);
-        assertEquals(0, GOLWorld.getEntities().size());
+        golWorld.removeEntity(e1);
+        assertEquals(0, golWorld.getEntities().size());
 
-        GOLWorld.removeEntity(e2);
-        assertEquals(0, GOLWorld.getEntities().size());
+        golWorld.removeEntity(e2);
+        assertEquals(0, golWorld.getEntities().size());
     }
 
     @Test
@@ -61,17 +62,52 @@ public class GOLWorldTest {
         GOLEntity e2 = new GOLEntity(new Vec2i(-2, 5));
         GOLEntity e3 = new GOLEntity(new Vec2i(2, 7));
         GOLEntity e4 = new GOLEntity(new Vec2i(-14, 12));
-        GOLWorld GOLWorld = new GOLWorld();
-        GOLWorld.addEntity(e1);
-        GOLWorld.addEntity(e2);
-        GOLWorld.addEntity(e3);
-        GOLWorld.addEntity(e4);
-        GOLWorld.addEntity(e3);
-        GOLWorld.addEntity(e2);
+        GOLWorld golWorld = new GOLWorld();
+        golWorld.addEntity(e1);
+        golWorld.addEntity(e2);
+        golWorld.addEntity(e3);
+        golWorld.addEntity(e4);
+        golWorld.addEntity(e3);
+        golWorld.addEntity(e2);
 
-        assertEquals(3, GOLWorld.getEntities().size());
-        assertTrue(GOLWorld.getEntities().contains(e1));
-        assertTrue(GOLWorld.getEntities().contains(e2));
-        assertTrue(GOLWorld.getEntities().contains(e4));
+        assertEquals(3, golWorld.getEntities().size());
+        assertTrue(golWorld.getEntities().contains(e1));
+        assertTrue(golWorld.getEntities().contains(e2));
+        assertTrue(golWorld.getEntities().contains(e4));
+    }
+
+    @Test
+    void testEntityAddNighbour() {
+        GOLEntity e1 = new GOLEntity(new Vec2i(2, 7));
+        GOLEntity e2 = new GOLEntity(new Vec2i(1, 6));
+        GOLEntity e3 = new GOLEntity(new Vec2i(2, 6));
+        GOLEntity e4 = new GOLEntity(new Vec2i(-2, 12));
+        GOLWorld golWorld = new GOLWorld();
+        golWorld.addEntity(e1);
+        golWorld.addEntity(e2);
+        golWorld.addEntity(e3);
+        golWorld.addEntity(e4);
+        golWorld.addEntity(e3);
+        golWorld.addEntity(e2);
+
+        assertEquals(2, e1.getNeighbours().size());
+        assertTrue(e1.getNeighbours().contains(e2));
+        assertTrue(e1.getNeighbours().contains(e3));
+        assertFalse(e1.getNeighbours().contains(e4));
+        assertFalse(e1.getNeighbours().contains(e1));
+
+        assertEquals(2, e2.getNeighbours().size());
+        assertTrue(e2.getNeighbours().contains(e1));
+        assertTrue(e2.getNeighbours().contains(e3));
+        assertFalse(e2.getNeighbours().contains(e4));
+        assertFalse(e2.getNeighbours().contains(e2));
+
+        assertEquals(2, e3.getNeighbours().size());
+        assertTrue(e3.getNeighbours().contains(e1));
+        assertTrue(e3.getNeighbours().contains(e2));
+        assertFalse(e3.getNeighbours().contains(e4));
+        assertFalse(e3.getNeighbours().contains(e3));
+
+        assertEquals(0, e4.getNeighbours().size());
     }
 }
